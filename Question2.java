5d80a3ffc67b61c53aa19cc41ce8301d6db1696d
NickGrajales <81117703+NickGrajales@users.noreply.github.com>
2023-02-09 14:12:00 -0500

    question 2
    java code for question 2

---
+++ Question2.java
@@ -0,0 +1,63 @@
+
+package question2;
+import javax.swing.*;
+
+public class Question2 {
+
+   
+    public static void main(String[] args) 
+    {
+        
+        String s1;
+        String s2;
+        String s3;
+        String s4;
+        String s5;
+        String s6;
+        double p1;
+        double p2;
+        double p3;
+        double average;
+
+        s1 = JOptionPane.showInputDialog("Input first item ");
+        s2 = JOptionPane.showInputDialog("What is the price ");
+        p1 = Double.parseDouble(s2);
+
+        s3 = JOptionPane.showInputDialog("Input the second item");
+        s4 = JOptionPane.showInputDialog("What is the price ");
+        p2 = Double.parseDouble(s4);
+
+        s5 = JOptionPane.showInputDialog("Input the third item");
+        s6 = JOptionPane.showInputDialog("What is the price ");
+        p3 = Double.parseDouble(s6);
+
+        average = (p1+p2+p3)/3;
+        
+         if (s1.equalsIgnoreCase("peas"))
+        {
+            System.out.println("No average input");
+        }
+        else if (s3.equalsIgnoreCase("peas"))
+        {
+            System.out.println("No average input");
+        }
+        else if (s5.equalsIgnoreCase("peas"))
+        {
+            System.out.println("No average input");
+        }
+        else
+        {
+        System.out.println("The names of the items are " + s1+"," + s3+ "," + s5 + " and the average is " + average);
+        } 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
